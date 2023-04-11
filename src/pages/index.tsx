7f1ec65d291523975252bfcdc104c7634af7fe81
Matheus Aragao <matheusaragaofs@gmail.com>
2023-04-11 08:58:45 -0300

    removing  map from index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,7 @@ import HeaderMenu from '@/components/HeaderMenu'
 import dynamic from 'next/dynamic'
 
 function HomePage() {
-  const Map = dynamic(
-    () => import('./../components/Map'),
-    { ssr: false }
-  )
+
   return <div className='d-flex bg-[#4811A2] h-screen items-center justify-center w-ful'>
     {/* <div className='text-3xl font-bold .'>Rastreie seu pet</div> */}
     <HeaderMenu showingOptions={true} />
